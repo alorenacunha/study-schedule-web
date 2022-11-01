ca72ac2168c37e1f8764ca6bb210eda8d91d08a1
alorenacunha <lorenacunha@outlook.com>
2022-11-01 16:21:12 -0300

    feat: :fire: remove old code

--- src/theme/ThemeProvider.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { ThemeProvider as StyledProvider } from "styled-components";
-
-const ThemeProvider = ({ children, theme }) => {
-  return <StyledProvider theme={theme}>{children}</StyledProvider>;
-};
-
-export default ThemeProvider;
